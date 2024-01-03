d39441f55e166ff9e2026dbb7af7d7fe9e15af83
HyeongSeok Kim <tiryul@gmail.com>
2024-01-03 14:57:08 +0900

    add: Header Class를 추가하다(WIP)

---
+++ openexr_python/__init__.py
@@ -0,0 +1,12 @@
+"""
+# OpenEXR-Python   
+
+Python package for OpenEXR
+
+For more information, Visit [the github repository](https://github.com/404Vector/OpenEXR-Python)
+ 
+"""
+
+__version__ = "0.0.0"  # dynamic
+
+from openexr_python.header import Header
