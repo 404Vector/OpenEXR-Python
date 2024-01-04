051669b69e7c2765c012e0a0b0196d32ddb532ba
HyeongSeok Kim <tiryul@gmail.com>
2024-01-04 12:03:12 +0900

    add: exr_enums을 추가하다*
    - image data type
    - line order type
    - compression type

---
+++ openexr_python/exr_enums/__init__.py
@@ -0,0 +1,4 @@
+# from .exr_enum_base import *
+from .exr_compression_type import *
+from .exr_image_data_type import *
+from .exr_line_order_type import *
---
+++ openexr_python/exr_enums/exr_enum_base.py
@@ -0,0 +1,7 @@
+from enum import Enum
+from typing import Any, Type
+from abc import *
+
+
+class EXREnumBase(Enum, metaclass=ABCMeta):
+    pass
